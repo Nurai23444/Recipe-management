5f81a7b46c742427d7da1ccea5e26d848d665d2d
Nurai23444 <123610954+Nurai23444@users.noreply.github.com>
2023-03-18 16:38:29 +0600

    Update ReceipeTag.cs

--- Recipe management/Recipe management/Entities/ReceipeTag.cs
+++ Recipe management/Recipe management/Entities/ReceipeTag.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public Recipes Recipe { get; set; }
         public Tags Tags { get; set; }
-
-
+        public int RecipeId { get; set; }
+        public int TagsId { get; set; }
     }
 }
